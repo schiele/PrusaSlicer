e8a2685c7bb4ff2c0a3d8c968d485923f495d156
Robert Schiele <rschiele@gmail.com>
2023-04-25 00:44:02 +0200

    make some packages blockable
    This enables a variable DEP_BLOCKED that allows blocking the packages
    Cereal, CGAL, CURL, EXPAT, GLEW, GMP, JPEG, MPFR, NLopt, OpenCSG,
    OpenEXR, OpenSSL, PNG, TIFF, and ZLIB for dependency build to allow a
    partial static build and take the blocked packages from the system.
    
    If DEP_BLOCKED is not set on the command line the dependency build keeps
    its previous behavior.

--- deps/OpenVDB/OpenVDB.cmake
+++ deps/OpenVDB/OpenVDB.cmake
@@ -10,7 +10,7 @@ orcaslicer_add_cmake_project(OpenVDB
     #  support vs2022, update to 8.2
     URL https://github.com/tamasmeszaros/openvdb/archive/a68fd58d0e2b85f01adeb8b13d7555183ab10aa5.zip 
     URL_HASH SHA256=f353e7b99bd0cbfc27ac9082de51acf32a8bc0b3e21ff9661ecca6f205ec1d81
-    DEPENDS dep_TBB dep_Blosc dep_OpenEXR dep_Boost
+    DEPENDS dep_TBB dep_Blosc ${OpenEXR_PKG} dep_Boost
     CMAKE_ARGS
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
         -DOPENVDB_BUILD_PYTHON_MODULE=OFF
