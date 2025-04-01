39a535c919fc3dcaa7dffd9b07607b129b5be469
Robert Schiele <rschiele@gmail.com>
2025-04-01 12:45:06 +0200

    update Boost
    Update to a more recent version 1.87.0.

--- deps/+Boost/Boost.cmake
+++ deps/+Boost/Boost.cmake
@@ -11,8 +11,8 @@ if (APPLE AND CMAKE_OSX_ARCHITECTURES)
 endif ()
 
 add_cmake_project(Boost
-    URL "https://github.com/boostorg/boost/releases/download/boost-1.83.0/boost-1.83.0.zip"
-    URL_HASH SHA256=9effa3d7f9d92b8e33e2b41d82f4358f97ff7c588d5918720339f2b254d914c6
+    URL "https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.zip"
+    URL_HASH SHA256=03530dec778bc1b85b070f0b077f3b01fd417133509bb19fe7c142e47777a87b
     LIST_SEPARATOR |
     CMAKE_ARGS
         -DBOOST_EXCLUDE_LIBRARIES:STRING=contract|fiber|numpy|stacktrace|wave|test
@@ -21,7 +21,6 @@ add_cmake_project(Boost
         -DBOOST_IOSTREAMS_ENABLE_ZSTD:BOOL=OFF
         "${_context_abi_line}"
         "${_context_arch_line}"
-        -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 )
 
 set(DEP_Boost_DEPENDS ZLIB)
