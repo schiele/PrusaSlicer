c846a5c9e21a0688707b20088b1226a92faed4a3
supermerill <merill@free.fr>
2025-07-23 20:18:57 +0200

    fix volume selection assert

--- src/slic3r/GUI/Selection.cpp
+++ src/slic3r/GUI/Selection.cpp
@@ -586,7 +586,7 @@ bool Selection::is_single_text() const
     const GLVolume* gl_volume = this->get_first_volume();
     if (gl_volume == nullptr || m_model->objects.size() <= gl_volume->object_idx() ||
         m_model->objects[gl_volume->object_idx()]->volumes.size() <= gl_volume->volume_idx()) {
-        assert(false); // shouldn't happen
+        // assert(false); // shouldn't happen // can happen on the wipetower
         return false;
     }
     const ModelVolume* model_volume = m_model->objects[gl_volume->object_idx()]->volumes[gl_volume->volume_idx()];
