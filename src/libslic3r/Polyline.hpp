9c492229e59ceb4ca580dad5b2008888dddbe4ef
supermerill <merill@free.fr>
2025-09-29 14:04:02 +0200

    fix scarf seam (extrude_loop_vase) with arc.

--- src/libslic3r/Polyline.hpp
+++ src/libslic3r/Polyline.hpp
@@ -357,18 +357,18 @@ public:
 
 
     // Works on points & arc
-    coordf_t              length() const { return Geometry::ArcWelder::path_length<coordf_t>(m_path); }
-    bool                  at_least_length(coordf_t length) const;
+    distf_t              length() const { return Geometry::ArcWelder::path_length<distf_t>(m_path); }
+    bool                  at_least_length(distf_t length) const;
     std::pair<int, Point> foot_pt(const Point &pt) const;
     void                  split_at(Point &point, ArcPolyline &p1, ArcPolyline &p2) const;
-    void                  split_at(coordf_t distance, ArcPolyline &p1, ArcPolyline &p2) const;
-    void                  clip_start(coordf_t dist);
-    void                  clip_end(coordf_t dist);
+    void                  split_at(distf_t distance, ArcPolyline &p1, ArcPolyline &p2) const;
+    void                  clip_start(distf_t dist);
+    void                  clip_end(distf_t dist);
     Polyline              to_polyline(coord_t deviation = 0) const;
     void                  translate(const Vector &vector);
     void                  rotate(double angle); // to test for arc, but should be okay
-    Point                 get_point_from_begin(coord_t distance) const;
-    Point                 get_point_from_end(coord_t distance) const;
+    Point                 get_point_from_begin(distf_t distance) const;
+    Point                 get_point_from_end(distf_t distance) const;
 
     // douglas_peuker and create arc if with_fitting_arc (don't touch the current arcs, only try in-between)
     void make_arc(ArcFittingType with_fitting_arc, coordf_t tolerance, double fit_percent_tolerance);
